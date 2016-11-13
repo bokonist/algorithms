a43b41a88a09d04d24d1748c48d904a01f38d9c4
bokonist <jayrm1995@gmail.com>
2016-11-13 14:09:12 +0530

    added some new macros to cpp-template

--- cpp-template.cpp
+++ cpp-template.cpp
@@ -1,11 +1,28 @@
 #include "bits/stdc++.h"
+
+/* useful utility macros */
 #define MIN(a,b,res) ((a)>(b))? (res)=(b):(res)=(a)
 #define MAX(a,b,res) ((a)<(b))? (res)=(b):(res)=(a)
-#define repeat(x) while(x--)
-#define nl cout<<"\n";
 #define printArray(a,l,n) for(int(i)=(l);(i)<int(n);i++) { cout<<(a[i])<<' ';}
+#define printWholeArray(a) for(int(i)=0;(i)<int((sizeof(a)/sizeof(a[0])));i++) { cout<<(a[i])<<' ';}
+#define repeat(n) for(int (i)=0;(i)<(int)(n);++(i)) //repeat n times
+#define repeatBounds(l,u) for(int (i)=(int)(l);(i)<(int)(u);++(i)) //repeat lower to upperbound
+#define until(x,val) for(int(i) = int(x); int(x)!=int(val); (i<val)?x++:x--) //until x becomes val through increment or decrement
+
+/*some constants*/
+static const long double PI = 3.142857143;
+
+/*some shorthands*/
+typedef long long ll;
+#define op cout<< // output to stdout
+#define ip cin>> //input from stdin
+#define err cerr<< //output to stderr
+#define nl cout<<"\n"; //newline
+
 using namespace std;
 int main()
 {
+	int x=5;
+
 	return 0;
 }
