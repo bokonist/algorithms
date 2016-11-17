c21ddf26c67fd07b40890322c6cebffec1140f8f
bokonist <jayrm1995@gmail.com>
2016-11-17 18:47:12 +0530

    implemented merge sort

---
+++ sorting/mergeSort.cpp
@@ -0,0 +1,93 @@
+#include "bits/stdc++.h"
+using namespace std;
+/* useful utility macros */
+#define MIN(a,b,res) ((a)>(b))? (res)=(b):(res)=(a)
+#define MAX(a,b,res) ((a)<(b))? (res)=(b):(res)=(a)
+#define printArray(a,l,n) for(int(i)=(l);(i)<int(n);i++) { cout<<(a[i])<<' ';}
+#define printWholeArray(a) for(int(i)=0;(i)<int((sizeof(a)/sizeof(a[0])));i++) { cout<<(a[i])<<' ';}
+#define repeat(n) for(int (i)=0;(i)<(int)(n);++(i)) //repeat n times
+#define repeatBounds(l,u) for(int (i)=(int)(l);(i)<(int)(u);++(i)) //repeat lower to upperbound
+#define until(x,val) for(int(i) = int(x); int(x)!=int(val); (i<val)?x++:x--) //until x becomes val through increment or decrement
+
+/*some constants*/
+static const long double PI = 3.142857143;
+
+/*some shorthands*/
+typedef long long ll;
+#define op cout<< // output to stdout
+#define ip cin>> //input from stdin
+#define err cerr<< //output to stderr
+#define nl cout<<"\n"; //newline
+
+void merge(int* a, int low, int mid, int high)
+{
+	int i, j, k;
+    int n1 = mid-low+1;
+    int n2 = high-mid;
+    int L[n1], R[n2];
+    for (i = 0; i < n1; i++)
+        L[i] = a[low + i];
+    for (j = 0; j < n2; j++)
+        R[j] = a[mid + 1 + j];
+    i = 0;
+    j = 0; 
+    k = low;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            a[k] = L[i];
+            i++;
+        }
+        else
+        {
+            a[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1)
+    {
+        a[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2)
+    {
+        a[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int* a,int low, int high)
+{
+	//op "sorting "<<low<<","<<high; nl
+	int mid=(low+high)/2;
+	if(low<high)
+	{
+		mergeSort(a,low,mid);
+		mergeSort(a,mid+1,high);
+		merge(a,low,mid,high);
+	}
+	
+}
+
+int main()
+{
+	int n=10;
+	op "Enter number of elements ";nl
+	//ip n;
+	int a[n];
+	for (int i = 0; i < n; ++i)
+	{
+		a[i]=rand()%100;
+	}
+	op "Unsorted array is \t: ";
+	printWholeArray(a); nl
+	mergeSort(a,0,n-1);
+	op "Sorted array is \t: ";
+	printWholeArray(a);nl
+	return 0;
+	return 0;
+}
